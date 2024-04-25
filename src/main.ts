c65bfed01c9424b6876d2da92262842e960b4963
zlatansmail <66694454+zlatansmail@users.noreply.github.com>
2024-04-25 16:40:13 +0200

    finished app

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/*
+*  Protractor support is deprecated in Angular.
+*  Protractor is used in this example for compatibility with Angular documentation tools.
+*/
+
+import { provideRouter } from '@angular/router';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import routeConfig from './app/routes';
+
+bootstrapApplication(AppComponent,
+  {
+    providers: [provideProtractorTestingSupport(),
+    provideRouter(routeConfig)
+    ]
+  })
+  .catch(err => console.error(err));
